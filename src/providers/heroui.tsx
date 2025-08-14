a5d64715510667de0ed5f2e3979f03533428b08e
fahry firdaus <ffahry169@gmail.com>
2025-08-15 02:11:09 +0700

    config: init heroui, acernity, reactbits

---
+++ src/providers/heroui.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import * as React from "react";
+
+// 1. import `HeroUIProvider` component
+import { HeroUIProvider } from "@heroui/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  // 2. Wrap HeroUIProvider at the root of your app
+  return (
+    <HeroUIProvider>
+        {children}
+    </HeroUIProvider>
+  );
+}

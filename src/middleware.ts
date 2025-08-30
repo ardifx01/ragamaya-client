ffb06142142ea87966cba9c1c6e1f7ce6f565414
vinss-droid <kevinsipahutar220604@gmail.com>
2025-08-30 23:52:11 +0700

    update middleware for protect dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,17 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import {GetUserData} from "@/lib/GetUserData";
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
 
     if (pathname.startsWith("/dashboard") && !request.cookies.has("access_token")) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const userRole = GetUserData().role;
+
+    if (userRole !== 'seller') {
         return NextResponse.redirect(new URL("/", request.url));
     }
 
